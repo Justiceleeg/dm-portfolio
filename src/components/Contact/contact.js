9d30a25928dbbbcf3a8b6b95d51437ac614d555a
Justice White <jaxingermany@Justice-Whites-MacBook-Pro.local>
2017-07-14 13:45:25 -0700

    initial completion

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import './contact.css';
 
+import discord from './../../imgs/discord.png';
+
 export default class Contact extends Component {
     
     render() {
@@ -9,9 +11,30 @@ export default class Contact extends Component {
             <div id="contact"  className="contact-container">
                 <div className="contact-contents">
                     <div className="title">Contact</div>
-                    <i className="fa fa-envelope" aria-hidden="true"></i>
-                    <i className="fa fa-linkedin" aria-hidden="true"></i>
-                    <i className="fa fa-slack" aria-hidden="true"></i>
+                    <div className="contacts">
+                        <div className="contact-box">
+                            <div className="contact-logo"><i className="fa fa-envelope" aria-hidden="true"></i></div>
+                            <div className="contact-name">Email</div>
+                            <div className="contact-username">justicepwhite<br/>@gmail.com</div>
+                        </div>
+                        <div className="contact-box">
+                            <div className="contact-logo"><a target="_blank" className="linkedin-link" href="https://www.linkedin.com/in/justiceperezwhite/">
+                                 <i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                            </div>
+                            <div className="contact-name">LinkedIn</div>
+                            <div className="contact-username">My Profile</div>
+                        </div>
+                        <div className="contact-box">
+                            <div className="contact-logo"><i className="fa fa-slack" aria-hidden="true"></i></div>
+                            <div className="contact-name">Slack</div>
+                            <div className="contact-username">@Justiceleeg</div>
+                        </div>
+                        <div className="contact-box">
+                            <div className="contact-logo"><img className="discord" src={discord} alt="Discord"/></div>
+                            <div className="contact-name">Discord</div>
+                            <div className="contact-username">@Justiceleeg</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
 import './projects.css';
+import cardiac from './../../imgs/Cardiac.jpg';
+import fiftyClone from './../../imgs/FiftyClone.jpg';
 
 export default class Projects extends Component {
     
@@ -9,6 +11,26 @@ export default class Projects extends Component {
             <div id="portfolio" className="projects-container">
                 <div className="projects-contents">
                     <div className="title">Portfolio</div>
+                    <div className="project-box">
+                        <a target="_blank" className="project-img-tag" href="http://cardiac.justicepwhite.com/"><img className="project-img" src={cardiac} alt="cardiac website"/></a>
+                        <div className="project-info">
+                            <span className="underlined">Cardiac - Nutrition Influenced Game</span>
+                            <br/>
+                            <li>React.js, Redux, Node.js, Express.js and PostgreSQL</li>
+                            <li>Created API endpoints on Node.js server and connected React.js app for CRUD operations</li>
+                            <li>Designed and built the About page, utilizing state change to affect CSS properties</li>
+                        </div>
+                    </div>
+                    <div className="project-box">
+                        <img className="project-img project-img-tag" src={fiftyClone} alt="cardiac website"/>
+                        <div className="project-info">
+                            <span className="underlined">Site Clone - Fifty/Fifty Bottles</span>
+                                <br/>
+                                <li>Angular 2/4, Node.js, Express.js and PostgreSQL</li>
+                                <li>Created relational database of products and their descriptions using PostgreSQL on Heroku</li>
+                                <li>Practiced best practices for Angular 2/4 apps by using proper module and component patterns</li>
+                        </div> 
+                    </div>
                 </div>
             </div>
         )
